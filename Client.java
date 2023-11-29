4fa986c2872fb50ea3f74e21be25f17ced259202
AbdoHosnie <abdohosnie@gmail.com>
2023-11-29 18:52:53 +0200

    Remove accounts list from Client class

--- Client.java
+++ Client.java
@@ -1,8 +1,5 @@
 package BankManagementSystem;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Client extends User {
     private final String accountNumber; // (unique)
     private final String username;
@@ -11,7 +8,6 @@ public class Client extends User {
     private final String accountState; //  (Active or Closed)
     private final String typeOfAccount; // (Savings or Current)
     private double balance;
-    private final List<Account> accounts;
 
     public Client(String ID, String firstName, String lastName, String accountNumber, String username, String password, String phoneNumber, String accountState, String typeOfAccount, double balance) {
         super(ID, firstName, lastName);
@@ -22,11 +18,12 @@ public class Client extends User {
         this.accountState = accountState;
         this.typeOfAccount = typeOfAccount;
         this.balance = balance;
-        this.accounts = new ArrayList<>();
     }
 
     public void editPersonalInformation(String newPhoneNumber) {
-
+        // Implement logic to edit personal information
+        this.phoneNumber = newPhoneNumber;
+        System.out.println("Personal information updated successfully.");
     }
 
     public String getID() {
@@ -40,20 +37,31 @@ public class Client extends User {
     }
 
     public void transferMoney(double amount, Account recipientAccount) {
-
+        // Implement logic for money transfer between accounts
+        // This may involve updating the balances of both the sender and recipient
     }
 
     public void showTransactionHistory() {
         // Implement transaction history display logic
     }
 
-    public void takeDeposit(double amount) {
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposit of $" + amount + " successful.");
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
 
-
+    public void withdraw(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrawal of $" + amount + " successful.");
+        } else {
+            System.out.println("Invalid withdrawal amount or insufficient funds.");
+        }
     }
 
     // You can add other methods as needed for the Client class
 }
-
-
-
